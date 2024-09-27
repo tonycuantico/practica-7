e215a4395c1a76a11c853db37d614b7ca4ddabd5
tonycuantico <159995704+tonycuantico@users.noreply.github.com>
2024-09-27 14:59:24 -0600

    Create NuevaCal.c

---
+++ NuevaCal.c
@@ -0,0 +1,44 @@
+/* Rodríguez Arrreguín Luis Antonio
+ejercicio 4 Calculadora con funciones
+practica 7 27/09/2024 */
+
+#include <stdio.h>
+#include <math.h>
+
+int resp;
+
+int suma (int a, int b)
+{
+  resp = a + b;
+  return(resp);
+}
+int resta(int a, intb)
+{
+  resp = a - b;
+  return (resp);
+}
+
+int mult(int a, int b)
+{
+  resp = a * b;
+  return (resp);
+}
+int div(int a, int b)
+{
+  resp = a / b
+  return(resp);
+}
+int exp(int a, int b)
+{
+  resp = pow(a,b);
+  return(resp);
+}
+
+int main()
+{
+  int a = 0, b = 0;
+  int respuesta, opcion;
+
+}
+  
+}
