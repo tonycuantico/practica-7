0c5627f34b8f59adf771377d78280996ffb7b9ca
tonycuantico <159995704+tonycuantico@users.noreply.github.com>
2024-09-27 13:43:36 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,15 @@
+Programa1.c
+#include<stdio.h>
+int main ()
+{
+  int a, b;
+  a = 3;
+  b = 2;
+  if (a > b)
+  {
+      printf("\ta (%d) es mayor a b (%d).\n", a,b);
+  }
+  printf("\t\vEl programa sigue su flujo.\n");
+  return 0;
+}
+
